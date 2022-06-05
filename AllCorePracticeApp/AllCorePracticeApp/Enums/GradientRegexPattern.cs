d260d9c763d88adbf4b6eeeeae8b2c639389e6fc
ReInvented <sescode@outlook.com>
2022-06-05 20:06:46 +0530

    RegEx for GradientStopsWithAngle with partially updated.

--- AllCorePracticeApp/AllCorePracticeApp/Enums/GradientRegexPattern.cs
+++ AllCorePracticeApp/AllCorePracticeApp/Enums/GradientRegexPattern.cs
@@ -4,7 +4,9 @@ namespace AllCorePracticeApp.Enums
 {
     public enum GradientRegexPattern
     {
-        [Description(@"Random")]
+        /// The RegEx used in this description is partial and only checks for integer values 0-360 with word boundaries
+        /// and also does not omit the negative values. So this needs to updated to cater for gradient stops and fractional portion of the angle.
+        [Description(@"\b(\d|\d{1,2}|[0-2]\d{1,2}|3(?:[0-5]\d|60))\b")]
         GradientStopsWithAngle,
 
         [Description(@"(#[0-9,A-F]{6,8}\s(?:0*(?:\.\d+)|1(?:\.0*)))+")]
