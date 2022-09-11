15fd59bc7087722a8792adad2efc218f4dd378b9
ReInvented <sescode@outlook.com>
2022-09-11 14:32:31 +0530

    Few new properties added to Flat class and Service class is added to generate the flats data.

--- ApartmentFinanceManager/ApartmentFinanceManager/Models/Flat.cs
+++ ApartmentFinanceManager/ApartmentFinanceManager/Models/Flat.cs
@@ -1,5 +1,7 @@
 ﻿using ApartmentFinanceManager.Enums;
+
 using ReInvented.Shared.Store;
+
 using System;
 using System.Collections.ObjectModel;
 
@@ -9,19 +11,50 @@ namespace ApartmentFinanceManager.Models
     {
         private readonly Block _block;
 
+        #region Constructors
+
         public Flat(Block block, string ownedBy)
+            : this(block, ownedBy, DateTime.Today)
+        {
+
+        }
+
+        public Flat(Block block, string ownedBy, DateTime accountOpenedOn)
         {
             _block = block;
             OwnedBy = ownedBy;
+            AccountOpenedOn = accountOpenedOn;
         }
+
+        public Flat(Block block, string ownedBy, string coOwnedBy)
+            : this(block, ownedBy, coOwnedBy, DateTime.Today)
+        {
+
+        }
+
+        public Flat(Block block, string ownedBy, string coOwnedBy, DateTime accountOpenedOn)
+        {
+            _block = block;
+            OwnedBy = ownedBy;
+            CoOwnedBy = coOwnedBy;
+            AccountOpenedOn = accountOpenedOn;
+        }
+
+        #endregion
+
         /// <summary>
         /// Flat number.
         /// </summary>
-        public int Number { get; set; }
+        public int Number { get => Get<int>(); set => Set(value); }
 
-        public string OwnedBy { get; set; }
+        public string OwnedBy { get => Get<string>(); set => Set(value); }
 
-        public ResidentType ResidentType { get; set; }
+        public string CoOwnedBy { get => Get<string>(); set => Set(value); }
+
+        public string TenantName { get => Get<string>(); set => Set(value); }
+
+
+        public ResidentType ResidentType { get => Get(ResidentType.Owner); set => Set(value); }
 
         /// <summary>
         /// Provides the complete description of the flat.
@@ -30,18 +63,18 @@ namespace ApartmentFinanceManager.Models
         /// <summary>
         /// Date on which the flat account is started.
         /// </summary>
-        public DateTime AccountOpenedOn { get; set; }
+        public DateTime AccountOpenedOn { get => Get<DateTime>(); set => Set(value); }
         /// <summary>
         /// Outstanding balance pending as of the date of opening this account.
         /// </summary>
-        public decimal OpeningBalance { get; set; }
+        public decimal OpeningBalance { get => Get<decimal>(); set => Set(value); }
         /// <summary>
         /// Keeps track of the all the expenses occured for this flat.
         /// </summary>
-        public ObservableCollection<Expense> Expenses { get; set; }
+        public ObservableCollection<Expense> Expenses { get => Get<ObservableCollection<Expense>>(); set => Set(value); }
         /// <summary>
         /// Keeps track of the all the payments made by the flat owner.
         /// </summary>
-        public ObservableCollection<Payment> Payments { get; set; }
+        public ObservableCollection<Payment> Payments { get => Get<ObservableCollection<Payment>>(); set => Set(value); }
     }
 }
---
+++ ApartmentFinanceManager/ApartmentFinanceManager/Services/GenerateBlockInitialData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static ApartmentFinanceManager.Enums.ResidentType;
+using ApartmentFinanceManager.Models;
+
+namespace ApartmentFinanceManager.Services
+{
+    internal static class GenerateBlockInitialData
+    {
+        public static Block Generate()
+        {
+            var block = new Block() { Name = "C" };
+
+            List<Flat> flats = new List<Flat>()
+            {
+                new Flat(block, "Sandeep A Deshpande") {Number = 017, OpeningBalance = 2800m},
+                new Flat(block, "Rajesh Singh") {Number = 018, OpeningBalance = 2800m},
+                new Flat(block, "Manjunath") {Number = 019, OpeningBalance = 2800m},
+                new Flat(block, "B V Nagaraja") {Number = 020, OpeningBalance = 2800m, ResidentType = Tenant, TenantName = "Manjunath K S"},
+                new Flat(block, "Satyanarayana", "Chaya G") {Number = 021, OpeningBalance = 2800m},
+                new Flat(block, "Ganapathi M K") {Number = 022, OpeningBalance = 2800m},
+                new Flat(block, "B V Chandrashekar") {Number = 023, OpeningBalance = 2800m, ResidentType = Tenant, TenantName = "Raghunath"},
+                new Flat(block, "B Mohan Sundaram", "Manika Lakshmi") {Number = 024, OpeningBalance = 2800m, ResidentType = Tenant},
+                new Flat(block, "B V Siddalingaswamy") {Number = 117, OpeningBalance = 2800m, ResidentType = Tenant},
+                new Flat(block, "B V Prasad") {Number = 118, OpeningBalance = 2800m, ResidentType = Tenant, TenantName = "Santhosh"},
+                new Flat(block, "Gangaiah", "Lalitha M R") {Number = 119, OpeningBalance = 2800m},
+                new Flat(block, "Shivanand Chawhan") {Number = 120, OpeningBalance = 2800m},
+                new Flat(block, "Girish") {Number = 121, OpeningBalance = 2800m},
+                new Flat(block, "B J Abhishek", "Bharani K") {Number = 122, OpeningBalance = 2800m},
+                new Flat(block, "Arun Joshi") {Number = 123, OpeningBalance = 2800m},
+                new Flat(block, "V S Lokeswarappa", "Manoj L") {Number = 124, OpeningBalance = 2800m},
+                new Flat(block, "Srinivasa Rao Masanam") {Number = 217, OpeningBalance = 2800m},
+                new Flat(block, "Laxmikanth Rao") {Number = 218, OpeningBalance = 2800m},
+                new Flat(block, "Nagendra Murthy M", "Shobha B") {Number = 219, OpeningBalance = 2800m},
+                new Flat(block, "Somala M", "Gopal N") {Number = 220, OpeningBalance = 2800m},
+                new Flat(block, "Bhavani", "Ravi Prakash") {Number = 221, OpeningBalance = 2800m},
+                new Flat(block, "B V Chandrashekar") {Number = 222, OpeningBalance = 2800m, ResidentType = Tenant, TenantName = "Harish"},
+                new Flat(block, "Praveen Gouda Patil") {Number = 223, OpeningBalance = 2800m},
+                new Flat(block, "Pradeepa K S", "Vaishnavi G R") {Number = 224, OpeningBalance = 2800m},
+                new Flat(block, "Santosh Kumar P", "Ashwini Krishnappa") {Number = 317, OpeningBalance = 2800m},
+                new Flat(block, "B V Chandrashekar") {Number = 318, OpeningBalance = 2800m, ResidentType = Tenant},
+                new Flat(block, "M G Harish Babu") {Number = 319, OpeningBalance = 2800m},
+                new Flat(block, "Shrikanth MH", "Jyothi HC") {Number = 320, OpeningBalance = 2800m},
+                new Flat(block, "Santosh P", "Nithya B") {Number = 321, OpeningBalance = 2800m},
+                new Flat(block, "Sai Shravanth") {Number = 322, OpeningBalance = 2800m},
+                new Flat(block, "Nikhil Kulkarni") {Number = 323, OpeningBalance = 2800m},
+                new Flat(block, "B V Nagaraja", "Deepa BN") {Number = 324, OpeningBalance = 2800m, ResidentType = Owner},
+
+            };
+
+
+            return new Block();
+        }
+    }
+}
