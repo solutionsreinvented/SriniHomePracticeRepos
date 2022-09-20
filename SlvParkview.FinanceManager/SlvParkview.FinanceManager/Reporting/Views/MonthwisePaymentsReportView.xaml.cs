ddb56643659e4ea8143f83c947e9490c8a5dc30f
ReInvented <sescode@outlook.com>
2022-09-20 06:34:20 +0530

    MonthwisePaymentsReportView added.

---
+++ SlvParkview.FinanceManager/SlvParkview.FinanceManager/Reporting/Views/MonthwisePaymentsReportView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SlvParkview.FinanceManager.Reporting.Views
+{
+    /// <summary>
+    /// Interaction logic for BlockOutstandingsReportView.xaml
+    /// </summary>
+    public partial class MonthwisePaymentsReportView : UserControl
+    {
+        public MonthwisePaymentsReportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
