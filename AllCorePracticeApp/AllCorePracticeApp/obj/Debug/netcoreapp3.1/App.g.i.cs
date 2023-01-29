2a69bfda6515c43085780bce2e60433530d27ddd
ReInvented <sescode@outlook.com>
2023-01-29 11:41:18 +0530

    C323 Payment updated.

--- AllCorePracticeApp/AllCorePracticeApp/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ AllCorePracticeApp/AllCorePracticeApp/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -47,7 +47,7 @@ namespace AllCorePracticeApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace AllCorePracticeApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             AllCorePracticeApp.App app = new AllCorePracticeApp.App();
             app.InitializeComponent();
