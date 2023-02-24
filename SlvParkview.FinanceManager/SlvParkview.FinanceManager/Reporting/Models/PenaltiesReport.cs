26ea26d3884e6f225ac0c6548515f514e0ec7abc
ReInvented <solutions.reinvented@gmail.com>
2023-02-24 10:40:24 +0530

    Penalties Report feature is being added.

---
+++ SlvParkview.FinanceManager/SlvParkview.FinanceManager/Reporting/Models/PenaltiesReport.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json;
+
+using ReInvented.DataAccess;
+using ReInvented.DataAccess.Interfaces;
+
+using SlvParkview.FinanceManager.Enums;
+using SlvParkview.FinanceManager.Extensions;
+using SlvParkview.FinanceManager.Models;
+using SlvParkview.FinanceManager.Reporting.Interfaces;
+using SlvParkview.FinanceManager.Reporting.Models.Base;
+using SlvParkview.FinanceManager.Services;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SlvParkview.FinanceManager.Reporting.Models
+{
+    /// <summary>
+    /// Creates a report that consists of flat wise outstanding (for a given block) calculated as on the specified date.
+    /// </summary>
+    public class PenaltiesReport : Report, IReport
+    {
+        #region Private Fields
+
+        private const string _fileName = "Penalties Summary";
+
+        private readonly Block _block;
+
+        private readonly DateTime _reportTill;
+
+        #endregion
+
+        #region Default Constructor
+
+        public PenaltiesReport()
+        {
+
+        }
+
+        #endregion
+
+        #region Parameterized Constructor
+
+        public PenaltiesReport(Block block, DateTime reportTill, OutstandingsFilter filter = OutstandingsFilter.All)
+        {
+            _block = block;
+            _reportTill = reportTill;
+
+            Filter = filter;
+        }
+
+        #endregion
+
+        #region Read-only Properties
+
+        [JsonProperty]
+        public List<FlatInfo> FlatInfoCollection { get => Get<List<FlatInfo>>(); private set => Set(value); }
+
+        [JsonProperty]
+        public string TotalOutstanding { get => Get<string>(); private set => Set(value); }
+
+        public OutstandingsFilter Filter { get => Get<OutstandingsFilter>(); private set => Set(value); }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void GenerateContents()
+        {
+            FlatInfoCollection = new List<FlatInfo>();
+
+            if (_block != null && _block.Flats != null)
+            {
+                foreach (Flat flat in _block.Flats)
+                {
+                    if (_block.ConsiderPenalties)
+                    {
+                        flat.GeneratePenalties(_block);
+                    }
+                    else
+                    {
+                        flat.Penalties.Clear();
+                    }
+
+                    flat.DateSpecified = _reportTill;
+
+                    if (Filter == OutstandingsFilter.Defaulters)
+                    {
+                        if (flat.OutstandingOnSpecifiedDate >= _block.MinimumOutstandingForPenalty)
+                        {
+                            FlatInfoCollection.Add(flat.ParseToFlatInfo());
+                        }
+                    }
+                    else
+                    {
+                        FlatInfoCollection.Add(flat.ParseToFlatInfo());
+                    }
+                }
+            }
+
+            TotalOutstanding = FlatInfoCollection?.Sum(f => decimal.Parse(StripNumberFormat(f.OutstandingOnSpecifiedDate))).FormatNumber("N1");
+        }
+
+        #endregion
+
+        #region Private Helper Methods
+
+        private protected override void CreateRequiredDirectories()
+        {
+            base.CreateRequiredDirectories();
+
+            /// Create the directory to store Block Outstandings Reports if it does not exists.
+            _reportTargetDirectory = Path.Combine(ServiceProvider.BlockOustandingsReportsDirectory);
+
+            if (!Directory.Exists(_reportTargetDirectory))
+            {
+                _ = Directory.CreateDirectory(_reportTargetDirectory);
+            }
+        }
+
+        private protected override void CreateHtmlFile()
+        {
+            string fileName = $"{GetFileName()}.html";
+
+            string[] htmlContents = File.ReadAllLines(Path.Combine(ServiceProvider.ReportTemplatesDirectory, $"{_fileName}.html"));
+
+            List<string> finalHtmlFileContent = ConcatenateScriptTagIn(htmlContents, fileName);
+
+            File.WriteAllLines(Path.Combine(_reportTargetDirectory, fileName), finalHtmlFileContent);
+        }
+
+        private protected override void CreateJavaScriptFile()
+        {
+            string fileName = $"{GetFileName()}.js";
+
+            string jsFilePath = Path.Combine(ServiceProvider.ReportScriptsDirectory, $"{_fileName}.js");
+            string[] jsContents = File.ReadAllLines(jsFilePath);
+
+            string finalJavaScriptFileContent = ConcatenateJsonContentIn(jsContents);
+
+            File.WriteAllText(Path.Combine(_reportTargetDirectory, fileName), finalJavaScriptFileContent);
+        }
+
+        private string StripNumberFormat(string value)
+        {
+            string unformattedString = value;
+
+            if (value.Contains("(") && value.Contains(")"))
+            {
+                unformattedString = string.Concat("-", value.Replace("(", "").Replace(")", ""));
+            }
+
+            return unformattedString;
+        }
+
+        #endregion
+
+        #region Private Helper Functions
+
+        private protected override string GetSerializedData()
+        {
+            IDataSerializer<PenaltiesReport> serializer = new JsonDataSerializer<PenaltiesReport>();
+
+            return serializer.Serialize(this);
+        }
+
+        private protected override string GetFileName()
+        {
+            var penaltyString = _block.ConsiderPenalties ? "With Penalties" : "Without Penalties";
+            return $"{_fileName} - As on {_reportTill:dd MMM yyyy} - {penaltyString} - {Filter}";
+        }
+
+        #endregion
+
+    }
+}
---
+++ SlvParkview.FinanceManager/SlvParkview.FinanceManager/Reporting/ViewModels/PenaltiesSummaryReportViewModel.cs
@@ -0,0 +1,52 @@
+﻿using SlvParkview.FinanceManager.Enums;
+using SlvParkview.FinanceManager.Reporting.Models;
+using SlvParkview.FinanceManager.ViewModels;
+
+using System;
+
+namespace SlvParkview.FinanceManager.Reporting.ViewModels
+{
+    /// <summary>
+    /// Reports the summary of total penalties applied on each flat calculated till the specified date.
+    /// </summary>
+    public class PenaltiesSummaryReportViewModel : ReportViewModelBase
+    {
+        #region Parameterized Constructor
+
+        public PenaltiesSummaryReportViewModel(SummaryViewModel summaryViewModel)
+            : base(summaryViewModel)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public DateTime ReportTill { get => Get<DateTime>(); set { Set(value); UpdateReport(); } }
+
+        public OutstandingsFilter Filter { get => Get(OutstandingsFilter.All); set { Set(value); UpdateReport(); } }
+
+        public string OutstandingHeader { get => Get<string>(); private set => Set(value); }
+
+        #endregion
+
+        #region Private Helpers
+
+        private protected override void Initialize()
+        {
+            base.Initialize();
+            ReportTill = DateTime.Today;
+            ///Filter = OutstandingsFilter.All;
+        }
+
+        private void UpdateReport()
+        {
+            OutstandingHeader = $"Outstanding as on {ReportTill:dd-MMM-yyyy}";
+            Report = new BlockOutstandingsReport(Block, ReportTill, Filter);
+            Report.GenerateContents();
+        }
+
+        #endregion
+    }
+}
