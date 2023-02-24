cb49c85fd371f89f8f67b63f5973846fba738724
ReInvented <solutions.reinvented@gmail.com>
2023-02-24 12:42:21 +0530

    EnumToDescriptionTypeConverter is used to provide proper category information in the Transactions History Report.
    Table styles changed to increase the width of the table itself and also the column dimensions of Transaction History table are adjusted to fit the descriptions properly.

--- SlvParkview.FinanceManager/SlvParkview.FinanceManager/Reporting/Models/TransactionInfo.cs
+++ SlvParkview.FinanceManager/SlvParkview.FinanceManager/Reporting/Models/TransactionInfo.cs
@@ -1,11 +1,15 @@
 ﻿using ReInvented.Shared.Stores;
+using ReInvented.Shared.TypeConverters;
 
+using SlvParkview.FinanceManager.Enums;
 using SlvParkview.FinanceManager.Extensions;
 
 namespace SlvParkview.FinanceManager.Reporting.Models
 {
     public class TransactionInfo : PropertyStore
     {
+        private static EnumToDescriptionTypeConverter _enumToDescriptionConverter = new EnumToDescriptionTypeConverter(typeof(TransactionCategory));
+
         public string TransactionDate { get => Get<string>(); set => Set(value); }
 
         public string PaymentAmount { get => Get<string>(); set => Set(value); }
@@ -24,15 +28,21 @@ namespace SlvParkview.FinanceManager.Reporting.Models
         {
             string blank = "-";
             string numberFormat = "N1";
-
+            ///var expenseCategory = _enumToDescriptionConverter.ConvertTo()
             return new TransactionInfo()
             {
                 TransactionDate = record.TransactionDate.ToString("dd MMM yyyy"),
                 PaymentAmount = record.Payment != null ? record.Payment.Amount.FormatNumber(numberFormat) : blank,
-                PaymentCategory = record.Payment != null ? record.Payment.Category.ToString() : blank,
+                PaymentCategory = 
+                record.Payment != null ?
+                _enumToDescriptionConverter.ConvertTo(record.Payment.Category, typeof(string)).ToString() : blank,
                 PaymentMode = record.Payment != null ? record.Payment.Mode.ToString() : blank,
                 ExpenseAmount = record.Expense != null ? record.Expense.Amount.FormatNumber(numberFormat) : blank,
-                ExpenseCategory = record.Expense != null ? record.Expense.Category.ToString() : blank,
+
+                ExpenseCategory = 
+                record.Expense != null ? 
+                _enumToDescriptionConverter.ConvertTo(record.Expense.Category, typeof(string)).ToString() : blank,
+
                 Outstanding = record.Outstanding.FormatNumber(numberFormat)
             };
         }
