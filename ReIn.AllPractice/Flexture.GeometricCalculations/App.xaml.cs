a1fe09bf46021a11049716b4cc252c0769e0d6a7
ReInvented <solutions.reinvented@gmail.com>
2023-05-08 12:21:16 +0530

    Swapping of windows is implemented using the Dispatcher

--- ReIn.AllPractice/Flexture.GeometricCalculations/App.xaml.cs
+++ ReIn.AllPractice/Flexture.GeometricCalculations/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Timers;
 using System.Windows;
 
+using Flexture.GeometricCalculations.ViewModels;
+
 using ReInvented.StaadPro.Interactivity.Entities;
 
 namespace Flexture.GeometricCalculations
@@ -16,20 +18,46 @@ namespace Flexture.GeometricCalculations
         {
             base.OnStartup(e);
 
-            MainWindow = new SplashScreenView();
-            MainWindow.Show();
+            /// Create and show the SplashScreen
 
-            //Member member1 = new Member(new Node() { X = 0.000, Y = 0.000, Z = 0.00 }, new Node() { X = 9.848, Y = 7.00, Z = -1.736 });
-            //Member member2 = new Member(new Node() { X = 7.878, Y = 0.00, Z = -1.389 }, new Node() { X = 0.000, Y = 10.00, Z = 0.00 });
+            /// Create a timer to close the SplashScreen after the specified time
+            Timer splashTimer = new Timer(3000);
 
-            //Node intersection = AIFunctions.LinesIntersection(member1, member2);
+            SplashScreenView splashScreenView = new SplashScreenView() { DataContext = new SplashScreenViewModel(splashTimer) };
+            splashScreenView.Show();
 
-            //Node rotatedNode = AIFunctions.RotateNode(member2.StartNode, member1.StartNode, member1.EndNode, 25.0 * Math.PI / 180.0);
+            splashTimer.Elapsed += (sender, args) =>
+            {
+                splashTimer.Stop();
 
+                /// Swap the Home with the WelcomeScreen
+                Dispatcher.Invoke(() =>
+                {
+                    MainWindow = new Home();
+                    MainWindow.Show();
 
-            //_ = MessageBox.Show($"Intersection node coordinates are X: {intersection.X:N3}; Y: {intersection.Y:N3}; Z: {intersection.Z:N3};.", "Intersect lines", MessageBoxButton.OK);
+                    /// Close the SplashScreen
+                    splashScreenView.Close();
+                });
+            };
+
+            splashTimer.Start();
+
+            //ChatBotFuctions();
 
         }
 
+        private static void ChatBotFuctions()
+        {
+            Member member1 = new Member(new Node() { X = 0.000, Y = 0.000, Z = 0.00 }, new Node() { X = 9.848, Y = 7.00, Z = -1.736 });
+            Member member2 = new Member(new Node() { X = 7.878, Y = 0.00, Z = -1.389 }, new Node() { X = 0.000, Y = 10.00, Z = 0.00 });
+
+            Node intersection = AIFunctions.LinesIntersection(member1, member2);
+
+            Node rotatedNode = AIFunctions.RotateNode(member2.StartNode, member1.StartNode, member1.EndNode, 25.0 * Math.PI / 180.0);
+
+
+            _ = MessageBox.Show($"Intersection node coordinates are X: {intersection.X:N3}; Y: {intersection.Y:N3}; Z: {intersection.Z:N3};.", "Intersect lines", MessageBoxButton.OK);
+        }
     }
 }
---
+++ ReIn.AllPractice/Flexture.GeometricCalculations/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Threading;
+
+
+namespace Flexture.GeometricCalculations.ViewModels
+{
+    public class SplashScreenViewModel
+    {
+        public SplashScreenViewModel(Timer timer)
+        {
+            Timer = timer;
+        }
+
+        public Timer Timer { get; set; }
+
+        public string RemainingTime => $"{Timer.Interval/1000}s Remaining";
+    }
+}
