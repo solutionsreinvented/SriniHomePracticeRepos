7cad72d1f7c81207fc084ea3fae444754d2e5450
ReInvented <solutions.reinvented@gmail.com>
2023-10-07 17:15:17 +0530

    Minor updates in Polygon

--- DesignStructs.TestingSuite/ReInvented.Geometry/Models/Polygon.cs
+++ DesignStructs.TestingSuite/ReInvented.Geometry/Models/Polygon.cs
@@ -14,7 +14,7 @@ namespace ReInvented.Geometry.Models
             return new HashSet<Point3D>()
             {
                 new Point3D(0, 0, 0), new Point3D(0, 5, 0), new Point3D(0, 10, 0), new Point3D(0, 15, 0), new Point3D(0, 20, 0), new Point3D(5, 20, 0),
-                new Point3D(10, 20, 0), new Point3D(20, 20, 0)///, new Point3D(20, 15, 0), new Point3D(20, 10, 0)
+                new Point3D(10, 20, 0), new Point3D(20, 20, 0), new Point3D(20, 15, 0), new Point3D(20, 10, 0)
             };
         }
     }
@@ -29,45 +29,13 @@ namespace ReInvented.Geometry.Models
 
         public IReadOnlyList<Point3D> Points { get; private set; }
 
-        public List<Point3D> ClosingLinePoints { get; private set; }
+        public double TotalIncludeAngle => CalculateTotalIncludedAngle();
+
+        public List<Point3D> ClosingLinePoints => GenerateClosingLinePoints();
 
         public Vector3DCollection Vectors { get; private set; }
 
-        public double TotalIncludeAngle => CalculateTotalIncludedAngle();
-
-        public void GenerateClosingPoints(double maxDimension = 5)
-        {
-            ClosingLinePoints = new List<Point3D>();
-
-            double lineLength = (Points.Last() - Points.First()).Length;
-            Vector3D lineVector = Vectors.Last();
-            lineVector.Normalize();
-
-            int nNewNodes = (int)(lineLength / maxDimension).Floor(1);
-            double adjustedDimension = lineLength / nNewNodes;
-
-            for (int i = 1; i < nNewNodes; i++)
-            {
-                var newVector = Vector3D.Multiply(i * adjustedDimension, lineVector);
-
-                ClosingLinePoints.Add(new Point3D(Points.Last().X + newVector.X, Points.Last().Y + newVector.Y, Points.Last().Z + newVector.Z));
-            }
-        }
-
         #region Private Helpers
-
-        private void GenerateVector3DCollection()
-        {
-            Vectors = new Vector3DCollection();
-
-            for (int i = 0; i < Points.Count(); i++)
-            {
-                Point3D startPoint = i == Points.Count() - 1 ? Points.Last() : Points[i];
-                Point3D endPoint = i == Points.Count() - 1 ? Points.First() : Points[i + 1];
-
-                Vectors.Add(new Vector3D(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y, endPoint.Z - startPoint.Z));
-            }
-        }
 
         private double CalculateTotalIncludedAngle()
         {
@@ -93,6 +61,40 @@ namespace ReInvented.Geometry.Models
             }
 
             return Math.Round(totalAngle, 1);
+        }
+
+        private List<Point3D> GenerateClosingLinePoints(double maxDimension = 5)
+        {
+            var closingLinePoints = new List<Point3D>();
+
+            double lineLength = (Points.Last() - Points.First()).Length;
+            Vector3D lineVector = Vectors.Last();
+            lineVector.Normalize();
+
+            int nNewNodes = (int)(lineLength / maxDimension).Floor(1);
+            double adjustedDimension = lineLength / nNewNodes;
+
+            for (int i = 1; i < nNewNodes; i++)
+            {
+                var newVector = Vector3D.Multiply(i * adjustedDimension, lineVector);
+
+                closingLinePoints.Add(new Point3D(Points.Last().X + newVector.X, Points.Last().Y + newVector.Y, Points.Last().Z + newVector.Z));
+            }
+
+            return closingLinePoints;
+        }
+
+        private void GenerateVector3DCollection()
+        {
+            Vectors = new Vector3DCollection();
+
+            for (int i = 0; i < Points.Count(); i++)
+            {
+                Point3D startPoint = i == Points.Count() - 1 ? Points.Last() : Points[i];
+                Point3D endPoint = i == Points.Count() - 1 ? Points.First() : Points[i + 1];
+
+                Vectors.Add(new Vector3D(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y, endPoint.Z - startPoint.Z));
+            }
         }
 
         #endregion
--- DesignStructs.TestingSuite/ReInvented.Geometry/Program.cs
+++ DesignStructs.TestingSuite/ReInvented.Geometry/Program.cs
@@ -12,7 +12,7 @@ namespace ReInvented.Geometry
         {
             Polygon polygon = new Polygon(PointsProvider.GetPoints().ToList());
 
-            polygon.GenerateClosingPoints();
+            polygon.GenerateClosingLinePoints();
 
             Arc arc = new Arc(new Point3D(6, 0, 1), new Point3D(5.330127019, 0, 3.5));
             arc.GetPropertiesInfo().ForEach(pi => Console.WriteLine(pi));
