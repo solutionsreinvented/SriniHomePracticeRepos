b03e0aebf8c5480032bc071efc8e3b6cbefc5bae
ReInvented <solutions.reinvented@gmail.com>
2023-10-07 17:53:57 +0530

    Triangle class created and testing valid triangles is in progress.

---
+++ DesignStructs.TestingSuite/ReInvented.Geometry/Models/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Media.Media3D;
+
+namespace ReInvented.Geometry.Models
+{
+    public class Triangle
+    {
+        public Triangle(Point3D p1, Point3D p2, Point3D p3)
+        {
+            Vertices = new Point3DCollection() { p1, p2, p3 };
+
+            CalculateProperties();
+        }
+
+
+        public Point3DCollection Vertices { get; private set; }
+
+        public Vector3DCollection Sides { get; private set; }
+
+        public double[] Angles { get; private set; }
+
+        public bool IsATriangle => !Angles.Any(a => a == 180.0 || a == double.NaN);
+
+        #region Private Helpers
+
+        private void CalculateProperties()
+        {
+            Sides = new Vector3DCollection
+            {
+                new Vector3D(Vertices[1].X - Vertices[0].X, Vertices[1].Y - Vertices[0].Y, Vertices[1].Z - Vertices[0].Z),
+                new Vector3D(Vertices[2].X - Vertices[1].X, Vertices[2].Y - Vertices[1].Y, Vertices[2].Z - Vertices[1].Z),
+                new Vector3D(Vertices[0].X - Vertices[2].X, Vertices[0].Y - Vertices[2].Y, Vertices[0].Z - Vertices[1].Z)
+            };
+
+            Angles = new double[]
+            {
+                Math.Round(Vector3D.AngleBetween(Sides[0], (-1) * Sides[2]), 1),
+                Math.Round(Vector3D.AngleBetween(Sides[1], (-1) * Sides[0]), 1),
+                Math.Round(Vector3D.AngleBetween(Sides[2], (-1) * Sides[1]), 1)
+            };
+
+        }
+
+        #endregion
+    }
+}
--- DesignStructs.TestingSuite/ReInvented.Geometry/Program.cs
+++ DesignStructs.TestingSuite/ReInvented.Geometry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Media.Media3D;
 
@@ -12,7 +13,29 @@ namespace ReInvented.Geometry
         {
             Polygon polygon = new Polygon(PointsProvider.GetPoints().ToList());
 
-            polygon.GenerateClosingLinePoints();
+            List<Point3D> closedPolygonPoints = polygon.Points.Union(polygon.ClosingLinePoints).ToList();
+
+            List<Point3D> remainingPoints = closedPolygonPoints;
+
+            do
+            {
+                for (int i = 0; i < remainingPoints.Count - 2; i++)
+                {
+                    var p1 = remainingPoints[i + 0];
+                    var p2 = remainingPoints[i + 1];
+                    var p3 = remainingPoints[i + 2];
+
+                    Triangle triangle = new Triangle(p1, p2, p3);
+
+                    if (triangle.IsATriangle)
+                    {
+
+                    }
+                }
+
+
+            } while (remainingPoints.Count > 0);
+
 
             Arc arc = new Arc(new Point3D(6, 0, 1), new Point3D(5.330127019, 0, 3.5));
             arc.GetPropertiesInfo().ForEach(pi => Console.WriteLine(pi));
