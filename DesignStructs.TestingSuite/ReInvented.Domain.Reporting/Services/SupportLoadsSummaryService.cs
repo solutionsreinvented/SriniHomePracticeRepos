2ee12d693096799bfcaccbe16508ae342c6446b2
ReInvented <solutions.reinvented@gmail.com>
2023-10-19 19:41:31 +0530

    SupportLoadsSummaryService implementation is started.

--- DesignStructs.TestingSuite/ReInvented.Domain.Reporting/Services/SupportLoadsSummaryService.cs
+++ DesignStructs.TestingSuite/ReInvented.Domain.Reporting/Services/SupportLoadsSummaryService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 using ReInvented.Domain.Reporting.Models;
+using ReInvented.StaadPro.Interactivity.Entities;
 
 namespace ReInvented.Domain.Reporting.Services
 {
@@ -8,7 +10,30 @@ namespace ReInvented.Domain.Reporting.Services
     {
         public static HashSet<LoadCaseForces> Generate(PCDLoads pcdLoads)
         {
-            
+            Node center = pcdLoads.SupportsInformation.Center;
+            List<SupportLoads> supportLoadsCollection = pcdLoads.SupportLoadsCollection.ToList();
+            int nSupports = supportLoadsCollection.Count();
+            int nLoadCaseCount = supportLoadsCollection[0].Loads.Count();
+
+            HashSet<LoadCaseForces> supportLoadsSummary = new HashSet<LoadCaseForces>();
+
+
+
+            for (int i = 0; i < nLoadCaseCount; i++)
+            {
+                List<LoadCaseForces> loads = supportLoadsCollection[i].Loads.ToList();
+
+                LoadCaseForces lcForces = new LoadCaseForces()
+                {
+                    Fx = supportLoadsCollection.Sum(slc => slc.Loads.ToList()[i].Fx),
+                    Fy = supportLoadsCollection.Sum(slc => slc.Loads.ToList()[i].Fy),
+                    Fz = supportLoadsCollection.Sum(slc => slc.Loads.ToList()[i].Fz),
+                    Mx = supportLoadsCollection.Sum(slc => slc.Loads.ToList()[i].Mx) + supportLoadsCollection.Sum(slc => (-1) * slc.Loads.ToList()[i].Fy * (slc.Support.Z - center.Z)),
+                    My = supportLoadsCollection.Sum(slc => slc.Loads.ToList()[i].My) + supportLoadsCollection.Sum(slc => 1 * slc.Loads.ToList()[i].Fy * (slc.Support.Z - center.Z))
+                }
+            }
+
+
             return null;
         }
     }
