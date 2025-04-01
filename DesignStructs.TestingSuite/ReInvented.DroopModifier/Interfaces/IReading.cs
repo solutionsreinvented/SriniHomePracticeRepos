784d8c2b44926d00364b82877a8c28cbdccdc7f0
ReInvented <solutions.reinvented@gmail.com>
2025-04-01 17:25:27 +0530

    DroopModifier project is moved to Main Solution

--- DesignStructs.TestingSuite/ReInvented.DroopModifier/Interfaces/IReading.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReInvented.DroopModifier.Interfaces
-{
-    public interface IReading
-    {
-        double Angle { get; set; }
-        double DroopDelta { get; set; }
-        double Radius { get; set; }
-    }
-}
