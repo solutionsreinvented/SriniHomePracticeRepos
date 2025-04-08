364977f515ef6738febe9fef0b3802fe202762bc
ReInvented <solutions.reinvented@gmail.com>
2025-04-08 14:04:33 +0530

    Multiple options is not working and hence reverted to the previous condition itself where Top and Bottom nodes are retrieved individually and directly instead of parsing.
    This however has to be modified again later.

--- DesignStructs.TestingSuite/ReInvented.UniformMesher/ViewModels/HomeViewModel.cs
+++ DesignStructs.TestingSuite/ReInvented.UniformMesher/ViewModels/HomeViewModel.cs
@@ -34,9 +34,9 @@ namespace ReInvented.UniformMesher.ViewModels
 
         public HashSet<Node> AllSelectedNodes { get => Get<HashSet<Node>>(); private set => Set(value); }
 
-        public HashSet<Node> TopNodes { get => Get<HashSet<Node>>(); private set => Set(value); }
+        public HashSet<Node> FirstNodeSet { get => Get<HashSet<Node>>(); private set => Set(value); }
 
-        public HashSet<Node> BottomNodes { get => Get<HashSet<Node>>(); private set => Set(value); }
+        public HashSet<Node> SecondNodeSet { get => Get<HashSet<Node>>(); private set => Set(value); }
 
         public HashSet<Plate> Plates { get => Get<HashSet<Plate>>(); private set => Set(value); }
 
@@ -57,7 +57,7 @@ namespace ReInvented.UniformMesher.ViewModels
                 FormationDirectionValues = Enum.GetValues(typeof(Direction)).Cast<Direction>()
                                                .Select(d => d.ToString())
                                                .Where(d => d != ProgressionDirection.ToString());
-                SelectedFormationDirection = FormationDirectionValues.FirstOrDefault();
+                SelectedFormationDirection = Direction.Vertical.ToString(); ///FormationDirectionValues.FirstOrDefault();
 
                 if (value != Direction.Circumferential)
                 {
@@ -95,9 +95,9 @@ namespace ReInvented.UniformMesher.ViewModels
 
         public ICommand BrowseSourceStaadFileCommand { get; private set; }
 
-        public ICommand GetTopNodesCommand { get; private set; }
+        public ICommand GetFirstNodeSetCommand { get; private set; }
 
-        public ICommand GetBottomNodesCommand { get; private set; }
+        public ICommand GetSecondNodeSetCommand { get; private set; }
 
         public ICommand GetAllSelectedNodesCommand { get; private set; }
 
@@ -114,48 +114,57 @@ namespace ReInvented.UniformMesher.ViewModels
             StaadModelPath = FileServiceProvider.GetFilePathUsingOpenFileDialog(new FileFilter("Staad model files", "std"));
             Wrapper = OpenStaadWrapperProvider.Get(StaadModelPath);
 
-            ValidModelIsSelected = string.IsNullOrEmpty(StaadModelPath) && Wrapper != null;
+            ValidModelIsSelected = !string.IsNullOrEmpty(StaadModelPath) && Wrapper != null;
         }
 
         private void OnGetSelectedNodes()
         {
             AllSelectedNodes = GetSelectedNodesExt(Wrapper.Geometry);
             NodesAreSelected = AllSelectedNodes != null && AllSelectedNodes.Count > 0;
+
+            _ = AllSelectedNodes == null || AllSelectedNodes.Count <= 0
+                ? MessageBox.Show("No nodes are selected", "Retrieve selected nodes", MessageBoxButton.OK)
+                : MessageBox.Show("Selected nodes are successfully retrieved", "Retrieve selected nodes", MessageBoxButton.OK);
         }
 
-        private void OnCreatePlatesInStaad()
+        private void OnGetFirsNodeSet()
         {
-            if (Wrapper != null && Plates != null && Plates.Count() > 0)
-            {
-                foreach (Plate p in Plates)
-                {
-                    Wrapper.Geometry.CreatePlate(p.Id, p.A.Id, p.B.Id, p.C.Id, p.D.Id);
-                }
-                _ = MessageBox.Show("All plates are created in Staad.", "Create Plates.", MessageBoxButton.OK);
-            }
-            else
-            {
-                _ = MessageBox.Show("An issue is encountered. No plates created.", "Create Plates.", MessageBoxButton.OK);
-            }
+            FirstNodeSet = GetSelectedNodesExt(Wrapper.Geometry).OrderBy(n => Node.PlanAngleIn360DegreesOf(n, Origin)).ToHashSet();
+            _ = FirstNodeSet == null || FirstNodeSet.Count <= 0
+                ? MessageBox.Show("No nodes are selected", "Retrieve selected nodes", MessageBoxButton.OK)
+                : MessageBox.Show("Selected nodes are successfully retrieved", "Retrieve selected nodes", MessageBoxButton.OK);
 
-            NodesAreSelected = false;
-            PlatesFormationCompleted = false;
+            NodesAreSelected = FirstNodeSet != null && FirstNodeSet.Count > 0 && SecondNodeSet != null && FirstNodeSet != SecondNodeSet;
+        }
+
+        private void OnGetSecondNodeSet()
+        {
+            SecondNodeSet = GetSelectedNodesExt(Wrapper.Geometry).OrderBy(n => Node.PlanAngleIn360DegreesOf(n, Origin)).ToHashSet();
+            _ = SecondNodeSet == null || SecondNodeSet.Count <= 0
+                ? MessageBox.Show("No nodes are selected", "Retrieve selected nodes", MessageBoxButton.OK)
+                : MessageBox.Show("Selected nodes are successfully retrieved", "Retrieve selected nodes", MessageBoxButton.OK);
+
+            NodesAreSelected = SecondNodeSet != null && SecondNodeSet.Count > 0 && FirstNodeSet != null && FirstNodeSet != SecondNodeSet;
         }
 
         private void OnFormPlateMesh()
         {
-            ParseTopAndBottomNodes();
+            //ParseTopAndBottomNodes();
 
-            if (Wrapper == null || TopNodes == null || BottomNodes == null)
+            if (Wrapper == null || FirstNodeSet == null || SecondNodeSet == null)
             {
                 _ = MessageBox.Show("An issue is encountered. No plate mesh is formed.", "Form plate mesh.", MessageBoxButton.OK);
             }
+            else if (FirstNodeSet.Count == SecondNodeSet.Count)
+            {
+                _ = MessageBox.Show("The number of nodes in the top nodes and bottom nodes set is different. No plate mesh is formed.", "Form plate mesh.", MessageBoxButton.OK);
+            }
             else
             {
-                if (TopNodes.Count == BottomNodes.Count)
+                if (FirstNodeSet.Count == SecondNodeSet.Count)
                 {
-                    List<Node> topNodesList = TopNodes.ToList();
-                    List<Node> botNodesList = BottomNodes.ToList();
+                    List<Node> topNodesList = FirstNodeSet.ToList();
+                    List<Node> botNodesList = SecondNodeSet.ToList();
                     int lastElemId = Math.Max(Wrapper.Geometry.GetLastBeamNo(), Wrapper.Geometry.GetLastPlateNo());
 
                     Plates = new HashSet<Plate>();
@@ -184,6 +193,49 @@ namespace ReInvented.UniformMesher.ViewModels
             PlatesFormationCompleted = Plates != null && Plates.Count > 0;
         }
 
+        private void OnCreatePlatesInStaad()
+        {
+            if (Wrapper != null && Plates != null && Plates.Count() > 0)
+            {
+                foreach (Plate p in Plates)
+                {
+                    Wrapper.Geometry.CreatePlate(p.Id, p.A.Id, p.B.Id, p.C.Id, p.D.Id);
+                }
+                _ = MessageBox.Show("All plates are created in Staad.", "Create Plates.", MessageBoxButton.OK);
+            }
+            else
+            {
+                _ = MessageBox.Show("An issue is encountered. No plates created.", "Create Plates.", MessageBoxButton.OK);
+            }
+
+            NodesAreSelected = false;
+            PlatesFormationCompleted = false;
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        private void Initialize()
+        {
+            Origin = new Node(0.0, 0.0, 0.0);
+            PlatesFormationCompleted = false;
+            ValidModelIsSelected = false;
+            NodesAreSelected = false;
+            ClosedMesh = true;
+            Tolerance = 0.005;
+
+            BrowseSourceStaadFileCommand = new RelayCommand(OnBrowseSourceStaadFile, true);
+
+            GetFirstNodeSetCommand = new RelayCommand(OnGetFirsNodeSet, true);
+            GetSecondNodeSetCommand = new RelayCommand(OnGetSecondNodeSet, true);
+
+            GetAllSelectedNodesCommand = new RelayCommand(OnGetSelectedNodes, true);
+            FormPlateMeshCommand = new RelayCommand(OnFormPlateMesh, true);
+            CreatePlatesInStaadCommand = new RelayCommand(OnCreatePlatesInStaad, true);
+            ProgressionDirection = Direction.Circumferential;
+        }
+
         private void ParseTopAndBottomNodes()
         {
             List<IGrouping<double, Node>> groupedNodes;
@@ -207,43 +259,9 @@ namespace ReInvented.UniformMesher.ViewModels
                     : AllSelectedNodes.GroupBy(n => Math.Round(Node.Radius(n, Origin), 3)).ToList();
             }
 
-            TopNodes = groupedNodes[1].Select(n => n).ToHashSet();
-            BottomNodes = groupedNodes[0].Select(n => n).ToHashSet();
+            FirstNodeSet = groupedNodes[1].Select(n => n).ToHashSet();
+            SecondNodeSet = groupedNodes[0].Select(n => n).ToHashSet();
         }
-
-        //private void OnGetBottomNodes()
-        //{
-        //    BottomNodes = GetSelectedNodesExt(Wrapper.Geometry).OrderBy(n => Node.PlanAngleIn360DegreesOf(n, Origin)).ToHashSet();
-        //    _ = MessageBox.Show("Bottom nodes are retrieval is completed.", "Get Bottom Nodes", MessageBoxButton.OK);
-        //}
-
-        //private void OnGetTopNodes()
-        //{
-        //    TopNodes = GetSelectedNodesExt(Wrapper.Geometry).OrderBy(n => Node.PlanAngleIn360DegreesOf(n, Origin)).ToHashSet();
-        //    _ = MessageBox.Show("Top nodes are retrieval is completed.", "Get Top Nodes", MessageBoxButton.OK);
-        //}
-
-        #endregion
-
-        #region Private Helpers
-
-        private void Initialize()
-        {
-            Origin = new Node(0.0, 0.0, 0.0);
-            PlatesFormationCompleted = false;
-            ValidModelIsSelected = false;
-            NodesAreSelected = false;
-            ClosedMesh = true;
-            Tolerance = 0.005;
-
-            BrowseSourceStaadFileCommand = new RelayCommand(OnBrowseSourceStaadFile, true);
-            GetAllSelectedNodesCommand = new RelayCommand(OnGetSelectedNodes, true);
-            FormPlateMeshCommand = new RelayCommand(OnFormPlateMesh, true);
-            CreatePlatesInStaadCommand = new RelayCommand(OnCreatePlatesInStaad, true);
-            ProgressionDirection = Direction.Circumferential;
-        }
-
-
 
         private HashSet<Node> GetSelectedNodesExt(OSGeometryUI geometry)
         {
